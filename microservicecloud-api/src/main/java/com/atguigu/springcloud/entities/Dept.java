74553f8bc388cc573f9c9a50020df32d9ff3e271
bells0 <41366442+bells0@users.noreply.github.com>
2019-12-10 08:52:52 +0800

    MircoService provider and consumer construct.

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -1,11 +1,15 @@
 package com.atguigu.springcloud.entities;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
-public class Dept {
+@NoArgsConstructor
+public class Dept implements Serializable {
 
-    private Long deptno; // 主键
+    private Integer deptno; // 主键
     private String 	dname; // 部门名称
     private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 
@@ -13,8 +17,11 @@ public class Dept {
     {
         super();
         this.dname = dname;
-
-
     }
 
+    public Dept(Integer deptno, String dname, String db_source) {
+        this.deptno = deptno;
+        this.dname = dname;
+        this.db_source = db_source;
+    }
 }
